37783918c8bc2d780cb7723c8e33a0f1c1fe557a
jackwhelan <jackw97@live.ie>
2020-01-30 21:54:30 +0000

    Add project files.

---
+++ SpeedMonCMD/Control.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace SpeedMonCMD
+{
+    class Control
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo("C:\\Users\\jackw\\Desktop\\Logs");
+            FileInfo[] Files = d.GetFiles("*.log");
+            foreach (FileInfo file in Files)
+            {
+                bool isPM;
+                if (file.Name.Split("_")[5].Contains("PM"))
+                {
+                    isPM = true;
+                }
+                else
+                {
+                    isPM = false;
+                }
+                Speedtest current = new Speedtest(System.Convert.ToInt32(file.Name.Split("_")[0]), System.Convert.ToInt32(file.Name.Split("_")[1]), System.Convert.ToInt32(file.Name.Split("_")[2]), System.Convert.ToInt32(file.Name.Split("_")[3]), System.Convert.ToInt32(file.Name.Split("_")[4]), isPM);
+                Console.WriteLine(current.getDate() + " | " + current.getTime());
+            }
+        }
+    }
+}
---
+++ SpeedMonCMD/Speedtest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedMonCMD
+{
+    class Speedtest
+    {
+        public int year { get; set; }
+        public int month { get; set; }
+        public int day { get; set; }
+        public int hour { get; set; }
+        public int minute { get; set; }
+        public bool isPM { get; set; }
+        public string ip { get; set; }
+        public string server { get; set; }
+        public float server_distance { get; set; }
+        public float ping { get; set; }
+        public float down { get; set; }
+        public float up { get; set; }
+
+        public Speedtest(int year, int month, int day, int hour, int minute, bool isPM, string ip, string server, float server_distance, float ping, float down, float up)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.hour = hour;
+            this.minute = minute;
+            this.isPM = isPM;
+            this.ip = ip;
+            this.server = server;
+            this.server_distance = server_distance;
+            this.ping = ping;
+            this.down = down;
+            this.up = up;
+        }
+
+        public Speedtest(int year, int month, int day, int hour, int minute, bool isPM)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.hour = hour;
+            this.minute = minute;
+            this.isPM = isPM;
+            this.ip = ip;
+            this.server = server;
+            this.server_distance = server_distance;
+            this.ping = ping;
+            this.down = down;
+            this.up = up;
+        }
+
+        public string getDate()
+        {
+            return this.day.ToString() + "/" + this.month.ToString() + "/" + this.year.ToString();
+        }
+
+        public string getTime()
+        {
+            string hour, minute;
+
+            if (this.hour < 10)
+            {
+                hour = "0" + this.hour.ToString();
+            }
+            else
+            {
+                hour = this.hour.ToString();
+            }
+
+            if (this.minute < 10)
+            {
+                minute = "0" + this.minute.ToString();
+            }
+            else
+            {
+                minute = this.minute.ToString();
+            }
+
+            if (this.isPM)
+            {
+                return hour + ":" + minute + "PM";
+            }
+            else
+            {
+                return hour + ":" + minute + "AM";
+            }
+        }
+    }
+}
